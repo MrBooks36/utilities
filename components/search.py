18ded13deda25440f4c7a7182981196414dfb43b
MrBooks36 <mrbooks36@outlook.com>
2025-03-26 08:16:27 +1000

    Update search.py

--- components/search.py
+++ components/search.py
@@ -1,40 +1,35 @@
-def findall(pattern, where="c:/", output=False):
+def findpaths(pattern, where="c:/", output=False):
  import os
  import fnmatch
- if os.path.isfile(where):
-  print('Not a folder: '+where)
+ if os.path.isfile(where): return('Not a folder: '+where)
  
  matches = []
  for root, dirnames, filenames in os.walk(where, topdown=False):
          for filename in fnmatch.filter(filenames, f'*{pattern}*'):
              matches.append(os.path.join(root, filename))
-             for file in matches:
-                if os.path.exists(os.path.join(root, file)) and output:
-                 print(os.path.join(root, file))
+             for file in matches: 
+                 if os.path.exists(os.path.join(root, file)) and output: print(os.path.join(root, file))
 
  return matches
 
 def findname(pattern, output=False, where="c:/"):
  import os
  import fnmatch
- if os.path.isfile(where):
-  print('Not a folder: '+where)
+ if os.path.isfile(where): return('Not a folder: '+where)
  
  matches = []
  for root, dirnames, files in os.walk(where, topdown=False):
          for files in fnmatch.filter(files, f'*{pattern}*'):
              matches.append(files)
-             for file in matches:
-                if os.path.exists(os.path.join(root, file)) and output:
-                 print(os.path.join(root, file))
+             for file in matches: 
+                 if os.path.exists(os.path.join(root, file)) and output: print(os.path.join(root, file))
 
  return matches
 
 def readall(pattern, output=False, where="c:/"):
     import os
     import fnmatch  
-    if os.path.isfile(where):
-       print('Not a folder: '+where)
+    if os.path.isfile(where): return('Not a folder: '+where)
     matches = []
     for root, dirnames, filenames in os.walk(where, topdown=False):
          for filename in filenames:
@@ -49,27 +44,21 @@ def readall(pattern, output=False, where="c:/"):
                           print(thing)
                           file.close
                   except Exception as e:
-                      if output:
-                          print(e)   
-                  if output:
-                   print(os.path.join(root, filename))
+                      if output: print(e)   
+                  if output: print(os.path.join(root, filename))
     return matches
 
    
-def countfiles(directory='c:/'):
+def countfiles(where='c:/'):
     import os 
-    if os.path.isfile(directory):
-     print(f'Not a folder: '+directory)
+    if os.path.isfile(where): return('Not a folder: '+where)
     file_count = 0
-    for root, dirs, files in os.walk(directory, topdown=False):
-        file_count += len(files)
+    for root, dirs, files in os.walk(where, topdown=False): file_count += len(files)
     return file_count
 
-def countfolders(directory='c:/'):
+def countfolders(where='c:/'):
     import os 
-    if os.path.isfile(directory):
-     print(f'Not a folder: '+directory)
+    if os.path.isfile(where): return('Not a folder: '+where)
     file_count = 0
-    for root, dirs, files in os.walk(directory, topdown=False):
-        file_count += len(files)
+    for root, dirs, files in os.walk(where, topdown=False): file_count += len(dirs)
     return file_count
