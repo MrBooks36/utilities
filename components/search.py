22b93fa19930cdcbf4b52af5d3763daf1164e685
MrBooks36 <mrbooks36@outlook.com>
2025-03-31 11:27:37 +1000

    Update search.py

--- components/search.py
+++ components/search.py
@@ -12,19 +12,21 @@ def findpaths(pattern, where="c:/", output=False):
 
  return matches
 
-def findname(pattern, output=False, where="c:/"):
- import os
- import fnmatch
- if os.path.isfile(where): return('Not a folder: '+where)
- 
- matches = []
- for root, dirnames, files in os.walk(where, topdown=False):
-         for files in fnmatch.filter(files, f'*{pattern}*'):
-             matches.append(files)
-             for file in matches: 
-                 if os.path.exists(os.path.join(root, file)) and output: print(os.path.join(root, file))
 
- return matches
+def findname(pattern, output=False, where="c:/"):
+    import os
+    import fnmatch
+    if os.path.isfile(where): 
+        return('Not a folder: ' + where)
+    
+    matches = []
+    for root, dirnames, filenames in os.walk(where, topdown=False):
+        for filename in fnmatch.filter(filenames, f'*{pattern}*'):
+            matches.append(os.path.join(root, filename))
+            if output: 
+                print(os.path.join(root, filename))
+
+    return matches
 
 def readall(pattern, output=False, where="c:/"):
     import os
